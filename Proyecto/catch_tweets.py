c5ed756b74c6ff05444d84fe3ae21600d3449260
Joseesc24 <josed.escobar@outlook.com>
2020-04-05 00:49:40 -0500

    Funciona, cambie el csv por un xml, por alguna razon el csv no quedaba bien escrito, el problema es que se queda pegado y jamas termina, no se porque :/

--- Proyecto/catch_tweets.py
+++ Proyecto/catch_tweets.py
@@ -1,7 +1,8 @@
-import csv
 import json
 from tweepy import Stream
+import lxml.etree as etree
 from tweepy import OAuthHandler
+from dicttoxml import dicttoxml
 from tweepy.streaming import StreamListener
 
 # --------------------------------------------------------------------------------
@@ -12,9 +13,8 @@ aTokenSecret = "V5LpcL3762uBpq7EeTuNbSN1qpDc7RKrj9jktFg2oh3s4"
 cKey = "8Jco2vpYtvvuj2UjeK75aQRWA"
 cSecret = "JsB5NFUFXueV5I2Oy2uPTuVpkMXFQV06XpIV1dpHQmNilWplMj"
 
-tweet_data = list()
+tweets_buffer_size = 40
 tweets_buffer = list()
-tweets_buffer_size = 30
 
 
 # --------------------------------------------------------------------------------
@@ -23,10 +23,8 @@ tweets_buffer_size = 30
 # --------------------------------------------------------------------------------
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        t = json.loads(data)
-        add_tweets_to_buffer(t)
-
-        return
+        tweet = json.loads(data)
+        add_tweets_to_buffer(tweet)
 
     def on_error(self, status):
         print(status)
@@ -37,66 +35,36 @@ class StdOutListener(StreamListener):
 # --------------------------------------------------------------------------------
 def add_tweets_to_buffer(tweet):
     global tweets_buffer
-    tweets_buffer.append(tweet)
-    if (len(tweets_buffer) == tweets_buffer_size):
-        add_tweets_to_csv_file(tweets_buffer)
-
-    return
+    if 'place' in [k for k in tweet] and tweet['place'] is not None:
+        tweets_buffer.append(tweet)
+    if len(tweets_buffer) == tweets_buffer_size:
+        add_tweets_to_xml_file(tweets_buffer)
 
 
 # --------------------------------------------------------------------------------
-# Write the captured tweets in a csv file called captured_tweets.csv
+# Write the captured tweets in a xml file called captured_tweets.xml
 # --------------------------------------------------------------------------------
-def add_tweets_to_csv_file(tweets):
-    with open('./catched_tweets.csv', 'w') as csvfile:
-        fieldnames = [
-            'id',
-            'text',
-            'screen_name',
-            'created_at',
-            'retweet_count',
-            'favorite_count',
-            'friends_count',
-            'followers_count',
-            'lang',
-            'country'
-        ]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+def add_tweets_to_xml_file(tweets):
+    tweet_data = dict()
 
-        for tweet in tweets:
-            if tweet['place'] is not None:
-                tweet_data.append({
-                    'id': tweet['id'],
-                    'text': tweet['text'],
-                    'screen_name': tweet['user']['screen_name'],
-                    'created_at': tweet['created_at'],
-                    'retweet_count': tweet['retweet_count'],
-                    'favorite_count': tweet['favorite_count'],
-                    'friends_count': tweet['user']['friends_count'],
-                    'followers_count': tweet['user']['followers_count'],
-                    'lang': tweet['lang'],
-                    'country': tweet['place']['country']
-                })
-                writer.writerows(tweet_data)
-                tweet_data.pop()
-            else:
-                tweet_data.append({
-                    'id': tweet['id'],
-                    'text': tweet['text'],
-                    'screen_name': tweet['user']['screen_name'],
-                    'created_at': tweet['created_at'],
-                    'retweet_count': tweet['retweet_count'],
-                    'favorite_count': tweet['favorite_count'],
-                    'friends_count': tweet['user']['friends_count'],
-                    'followers_count': tweet['user']['followers_count'],
-                    'lang': tweet['lang'],
-                    'country': 'None'
-                })
-            writer.writerows(tweet_data)
-            tweet_data.pop()
+    for tweet in tweets:
+        tweet_id = str(tweet['id'])
+        if tweet_id not in tweet_data:
+            tweet_data[tweet_id] = {
+                'text': tweet['text'],
+                'screen_name': tweet['user']['screen_name'],
+                'created_at': tweet['created_at'],
+                'retweet_count': tweet['retweet_count'],
+                'favorite_count': tweet['favorite_count'],
+                'friends_count': tweet['user']['friends_count'],
+                'followers_count': tweet['user']['followers_count'],
+                'lang': tweet['lang'],
+                'country': tweet['place']['country']
+            }
 
-        return False
+    xml_output = dicttoxml(tweet_data, custom_root='tweets', attr_type=False)
+    tree = etree.fromstring(xml_output)
+    tree.getroottree().write("catched_tweets.xml", pretty_print=True)
 
 
 my_listener = StdOutListener()
