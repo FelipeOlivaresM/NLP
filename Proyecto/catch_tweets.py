66f193d25af5850432257430aeed4dc4a7325958
Joseesc24 <josed.escobar@outlook.com>
2020-04-05 19:05:50 -0500

    Trate de separarlo por archivos para ir limpiando el buffer a medida que se escribe y agregue la barra de progreso, pero no se porque termina todo al tiempo :/

--- Proyecto/catch_tweets.py
+++ Proyecto/catch_tweets.py
@@ -1,27 +1,46 @@
 import json
-import xmltodict
 import dicttoxml
+import xmltodict
 import progressbar
 from tweepy import Stream
 import lxml.etree as etree
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
 
+'''current_xmlf = open('catched_tweets.xml', 'rb')
+current_data_ids = [k for k in dict(xmltodict.parse(current_xmlf)['tweets'])]
+current_xmlf.close()'''
+
 
 class Listener(StreamListener):
-    def __init__(self, tweets_buffer_size=10, tweets_buffer=dict()):
+    def __init__(self, tweets_buffer_size=100, tweets_per_file=100):
+        self.bar = progressbar.ProgressBar(maxval=tweets_buffer_size,
+                                           widgets=[progressbar.Bar('=', '[', ']'), ' ', progressbar.Percentage()])
         self.tweets_buffer_size = tweets_buffer_size
-        self.tweets_buffer = tweets_buffer
+        self.tweets_per_file = tweets_per_file
+        self.output_path = 'catched_tweets'
+        self.tweets_buffer = dict()
+        self.writed_tweets = 0
+        self.num_file = 0
+        self.bar.start()
 
     def on_data(self, data):
         tweet = json.loads(data)
         if 'place' in [k for k in tweet] and tweet['place'] is not None and tweet['retweet_count'] is 0:
             self.add_tweet_to_buffer(tweet)
-            return
+            self.writed_tweets += 1
+            self.bar.update(self.writed_tweets)
 
-        if len([k for k in self.tweets_buffer]) == self.tweets_buffer_size:
-            self.add_tweets_to_xml_file()
-            return False
+            if self.writed_tweets == self.tweets_buffer_size:
+                self.add_tweets_to_xml_file()
+                self.tweets_buffer.clear()
+                self.bar.finish()
+                return False
+
+            elif len([k for k in self.tweets_buffer]) == self.tweets_per_file:
+                self.add_tweets_to_xml_file()
+                self.tweets_buffer.clear()
+                self.num_file += 1
 
     def add_tweet_to_buffer(self, tweet):
         tweet_id = str(tweet['id'])
@@ -40,7 +59,8 @@ class Listener(StreamListener):
     def add_tweets_to_xml_file(self):
         xml_output = dicttoxml.dicttoxml(self.tweets_buffer, custom_root='tweets', attr_type=False)
         tree = etree.fromstring(xml_output)
-        tree.getroottree().write("catched_tweets.xml", pretty_print=True)
+        path = str(self.output_path) + "_" + str(self.num_file) + ".xml"
+        tree.getroottree().write(path, pretty_print=True, encoding='UTF-8')
 
     def on_error(self, status):
         print("Error ", status)
@@ -56,16 +76,18 @@ cKey = "8Jco2vpYtvvuj2UjeK75aQRWA"
 cSecret = "JsB5NFUFXueV5I2Oy2uPTuVpkMXFQV06XpIV1dpHQmNilWplMj"
 
 # --------------------------------------------------------------------------------
-# Get current tweet ids in the xml file.
-# --------------------------------------------------------------------------------
-current_xmlf = open('catched_tweets.xml', 'rb')
-current_data_ids = [k for k in dict(xmltodict.parse(current_xmlf)['tweets'])]
-current_xmlf.close()
-
-# --------------------------------------------------------------------------------
 # Parameters for the listener.
+
+# tweets_per_file = number of tweets that are going to be written in one xml file,
+#                   this number is also the number of tweets that the ram memory
+#                   need to handle while writing them in the disc,if you have
+#                   problems with the ram memory space maybe by decreasing this
+#                   number could solve it.
+
+# tweets_buffer_size = number of tweets that ar going to be catched.
 # --------------------------------------------------------------------------------
 listener = Listener()
+listener.tweets_per_file = 2
 listener.tweets_buffer_size = 2
 
 # --------------------------------------------------------------------------------
