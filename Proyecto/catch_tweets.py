d51ddcd984a618ffcb562717e67dd7c4af7b6fe2
Joseesc24 <josed.escobar@outlook.com>
2020-04-07 08:05:27 -0500

    Ya saque los datos, estoy tratandeo de arreglar lo de los archivos

--- Proyecto/catch_tweets.py
+++ Proyecto/catch_tweets.py
@@ -1,8 +1,8 @@
+import csv
 import json
 import time
-from datetime import datetime
+import walk
 
-import pandas as pd
 from tweepy import OAuthHandler
 from tweepy import Stream
 from tweepy.streaming import StreamListener
@@ -14,22 +14,19 @@ tags = [
     'CoronaVirus', 'SARSCoV2', 'SARS-CoV2', 'SARS-CoV-2'
 ]
 
-tweets_buffer = pd.DataFrame(columns=[
-    'id', 'text', 'screen_name', 'created_at', 'retweet_count',
-    'favorite_count', 'friends_count', 'followers_count', 'lang',
-    'country'
-])
-
-number_of_tweets_for_catch = 2000  # <----- Numero de tweets en total.
-tweets_per_file = 500  # <----- Numero de tweets por archivo.
-writed_tweets = 0
-
 start_time = time.time()
+number_of_tweets_for_catch = 2  # <----- Numero de tweets en total.
+tweet_as_list = list()
+writed_tweets = 0
 
 
 class Listener(StreamListener):
     def on_data(self, data):
-        return process_incoming_data(json.loads(data))
+        process_incoming_data(json.loads(data))
+        if writed_tweets == number_of_tweets_for_catch:
+            return False
+        else:
+            return
 
     def on_error(self, status):
         return False
@@ -37,65 +34,66 @@ class Listener(StreamListener):
 
 def process_incoming_data(tweet):
     global number_of_tweets_for_catch
-    global tweets_per_file
-    global tweets_buffer
     global writed_tweets
     global tags
 
     if 'place' in [k for k in tweet] and tweet['place'] is not None and not tweet['retweeted']:
         if 'RT @' not in tweet['text'] and any(tag in tweet['text'] for tag in tags):
-
-            add_tweet_to_buffer(tweet)
+            tweet_to_list(tweet)
+            add_tweets_to_csv_file()
             writed_tweets += 1
 
-            files_in_buffer = len(tweets_buffer.index)
-
             print(
-                "Tweets en el buffer: " +
-                str(files_in_buffer) + " de " +
-                str(tweets_per_file) +
-                " - Tweets capturados: " +
+                " Tweets capturados: " +
                 str(writed_tweets) + " de " +
                 str(number_of_tweets_for_catch) +
                 " - Tiempo de ejecucion: " + str(int((time.time() - start_time) / 60)) +
                 " minutos."
             )
 
-            if writed_tweets == number_of_tweets_for_catch:
-                add_tweets_to_csv_file()
-                tweets_buffer = tweets_buffer.drop([0])
-                print("Proceso terminado.\n")
-                return False
 
-            elif files_in_buffer == tweets_per_file:
-                add_tweets_to_csv_file()
-                tweets_buffer = tweets_buffer.drop([0])
-                print("Buffer reiniciado.\n")
-                return
-
-
-def add_tweet_to_buffer(tweet):
-    global tweets_buffer
-    tweets_buffer = tweets_buffer.append(pd.DataFrame({
-        'id': [tweet['id']],
-        'text': [tweet['text'].strip().replace("\n", " ")],
-        'screen_name': [tweet['user']['screen_name']],
-        'created_at': [tweet['created_at']],
-        'retweet_count': [tweet['retweet_count']],
-        'favorite_count': [tweet['favorite_count']],
-        'friends_count': [tweet['user']['friends_count']],
-        'followers_count': [tweet['user']['followers_count']],
-        'lang': [tweet['lang']],
-        'country': [tweet['place']['country']]
-    }))
+def tweet_to_list(tweet):
+    global tweet_as_list
+    tweet_as_list = [
+        tweet['id'],
+        tweet['text'].strip().replace("\n", " "),
+        tweet['user']['screen_name'],
+        tweet['created_at'],
+        tweet['retweet_count'],
+        tweet['favorite_count'],
+        tweet['user']['friends_count'],
+        tweet['user']['followers_count'],
+        tweet['lang'],
+        tweet['place']['country']
+    ]
 
 
 def add_tweets_to_csv_file():
-    global tweets_buffer
-    path = str(len(tweets_buffer.index)) + "_catched_tweets_(" + str(
-        datetime.now().strftime("%H:%M:%S - %d-%m-%Y")) + ").csv"
-    tweets_buffer.to_csv(path, encoding='utf-8', index=False)
-    print("\nEscritura del archivo " + path + " terminada.")
+    global tweet_as_list
+    path_output = "catched_tweets_1.csv"
+    path_1, subfolders, files_list = list(walk('./'))
+    if path_output not in files_list:
+        csv_file = open(path_output, 'a', encoding="utf-8")
+        writer = csv.writer(csv_file)
+        writer.writerow(
+            ['id',
+             'text',
+             'screen_name',
+             'created_at',
+             'retweet_count',
+             'favorite_count',
+             'friends_count',
+             'followers_count',
+             'lang',
+             'country'
+             ]
+        )
+        csv_file.close()
+    if path_output in files_list:
+        csv_file = open(path_output, 'a', encoding="utf-8")
+        writer = csv.writer(csv_file)
+        writer.writerow(tweet_as_list)
+        csv_file.close()
 
 
 # --------------------------------------------------------------------------------
@@ -113,7 +111,8 @@ authenticator = OAuthHandler(cKey, cSecret)
 authenticator.set_access_token(aToken, aTokenSecret)
 
 print("")
-while writed_tweets is not number_of_tweets_for_catch:
+while writed_tweets <= number_of_tweets_for_catch:
+    print("Reinicinando")
     try:
         stream = Stream(authenticator, Listener())
         stream.filter(languages=['en', 'es'], track=tags)
