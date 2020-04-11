2f76218edef296ad2557ef11641eb31453e310ad
Joseesc24 <josed.escobar@outlook.com>
2020-04-11 09:08:54 -0500

    Nuevos datos

--- Proyecto/lab.py
+++ Proyecto/lab.py
@@ -6,7 +6,7 @@ from urllib3.exceptions import ProtocolError
 from requests.exceptions import Timeout, ConnectionError
 import re, csv, sys, json, time, random, threading
 
-output_path = "./twitter_data/catched_tweets_1.csv"  # <----- Ruta de salida para el archivo, el archivo 4 es para pruebas.
+output_path = "./twitter_data/catched_tweets_4.csv"  # <----- Ruta de salida para el archivo, el archivo 4 es para pruebas.
 number_of_tweets_for_catch = 3000  # <----- Numero de tweets en total.
 start_time = time.time()
 lock = threading.Lock()
